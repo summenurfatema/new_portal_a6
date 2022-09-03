a202f93f735ef60066647a64ae8934e28098d356
Fatema04 <shadrinmoni15@gmail.com>
2022-09-03 13:14:25 -0700

    100%

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-console.log('clicked')
 
-// load category
+
+//---------------------------- Load Categories--------------------------//
 
 const loadCategory = async () => {
     try {
@@ -15,7 +15,10 @@ const loadCategory = async () => {
     }
 
 }
-// display catagory
+
+//-------------------------------display catagory----------------------------//
+
+
 const displayCategories = (categories) => {
     console.log(categories)
     const categoryContainer = document.getElementById('category-container');
@@ -25,40 +28,42 @@ const displayCategories = (categories) => {
 
         const categoryDiv = document.createElement('div');
 
-
         categoryDiv.innerHTML = `${category.category_name}`;
         categoryDiv.onclick = function () {
             loadArticals(`${category.category_id}`)
             toggleSpiner(true)
-
-
         }
         categoryContainer.appendChild(categoryDiv);
-
 
     }
 
 }
 
 
-//  load articals
+// ------------------------------Load All Articals----------------------------------//
+
+
 const loadArticals = async (category_id) => {
 
-    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
-    const res = await fetch(url)
-    const data = await res.json()
-    // console.log(data.data)
-    displayArticals(data.data);
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
+        const res = await fetch(url)
+        const data = await res.json()
+        displayArticals(data.data);
+    } catch (error) {
+        loadArticals([])
+    }
 }
 
+// -----------------------------Display All Article------------------------//
 
 const displayArticals = (articals) => {
-    //console.log(articals)
-    //sorted
+
+    //------Sort------//
+
     articals.sort(function (a, b) {
         return b.total_view - a.total_view;
     });
-
 
     const number = document.getElementById('number-of-news')
     if (articals.length !== 0) {
@@ -72,7 +77,6 @@ const displayArticals = (articals) => {
 
 
     for (const article of articals) {
-        //console.log(article)
 
         const artDiv = document.createElement('div')
         artDiv.classList.add('col')
@@ -80,22 +84,22 @@ const displayArticals = (articals) => {
         artDiv.innerHTML = `
 
         <div class="card m-3">
-        <img src="${article.thumbnail_url}" class="card-img-top h-50" alt="...">
+            <img src="${article.thumbnail_url}" class="card-img-top h-50" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${article.title}</h5>
-          <p id="des" class="title-text mb-3">${article.details}</p>
+            <h5 class="card-title">${article.title}</h5>
+            <p id="des" class="title-text mb-3">${article.details}</p>
 
 
         <div class="d-flex">
 
             <div class="d-flex">
                 <div>
-                <img style="width:50px;height:50px" class="border rounded-circle" src="${article.author.img}">
+                    <img style="width:50px;height:50px" class="border rounded-circle" src="${article.author.img}">
                 </div>
 
                 <div>
-                <p>${article.author.name}</p>
-                <p>${article.author.published_date}</p>
+                    <p>${article.author.name}</p>
+                    <p>${article.author.published_date}</p>
                 </div>
             </div>
 
@@ -103,64 +107,53 @@ const displayArticals = (articals) => {
             <div><p><i class="fa-sharp fa-solid fa-eye"></i>${article.total_view}</p></div>
 
             <div class="ms-4">
-            <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star-half-stroke"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star-half-stroke"></i>
             </div>
                 
         </div>
+
         <button onclick="openModal('${article._id}')" type="button" class="btn btn-info text-white fw-semibold mt-1"
         data-bs-toggle="modal" data-bs-target="#exampleModal">
         See details
     </button>
-        </div>
 
-
-
-            
+        </div>        
 `;
         artContainer.appendChild(artDiv)
     }
 
-
     toggleSpiner(false)
 
-
-
 }
-//details
+//-------------------------Modal Section----------------------------------//
 
 const openModal = async (news_id) => {
-    //console.log(news_id)
-    const url = `https://openapi.programming-hero.com/api/news/${news_id}`
-    const res = await fetch(url)
-    const data = await res.json()
 
-    displayModal(data);
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/${news_id}`
+        const res = await fetch(url)
+        const data = await res.json()
+
+        displayModal(data);
+
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+
 const displayModal = newses => {
-
-    //sort
-    // function sorted(a, b) {
-    //     if (a.total_view > b.total_view) {
-    //         return 1;
-
-    //     }
-    //     else { return -1 }
-
-    // }
-
-    // console.log(newses.data[0])
-
-
-
-
 
     const modalTitle = document.getElementById('exampleModalLabel');
     modalTitle.innerText = ` ${newses.data[0].title}`
     const modalbody = document.getElementById('modal-body1');
+
+
+    // ----------------All error handle for null value------------------//
 
     if (newses.data[0].author.name === '' || newses.data[0].author.name === null) {
         newses.data[0].author.name = 'No Author found'
@@ -183,15 +176,13 @@ const displayModal = newses => {
     <p>Author name : ${newses.data[0].author.name} </p>
     <p>Publish date & time : ${newses.data[0].author.published_date} </p>
     <p>Total view : ${newses.data[0].total_view} </p>
-
-    
     <p>Details of this news : ${newses.data[0].details}</p>
     
       `
 
 }
 
-//spinner
+//-------------------------Spinner Function------------------------------//
 
 const toggleSpiner = (isloading) => {
     const loader = document.getElementById('loader');
@@ -202,7 +193,7 @@ const toggleSpiner = (isloading) => {
     else { loader.classList.add('d-none') }
 }
 
-//sort
+
 
 
 loadCategory()
